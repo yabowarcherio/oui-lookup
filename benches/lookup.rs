ecef2cd2659684325ed0aca10493d72974def6fa
yabowarcherio <yabowarcher8590@gmail.com>
2026-06-13 20:30:00 +0300

    bench: add scope() and lookup_oui_octets micro-benches

--- benches/lookup.rs
+++ benches/lookup.rs
@@ -1,7 +1,7 @@
 //! Micro-benchmark for the hot lookup path.
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use oui_lookup::{classify, entries, lookup, parse_mac48, search};
+use oui_lookup::{classify, entries, lookup, lookup_oui_octets, parse_mac48, scope, search};
 
 fn bench_lookup(c: &mut Criterion) {
     // A spread of well-known, registered OUIs.
@@ -26,6 +26,20 @@ fn bench_lookup(c: &mut Criterion) {
             let m = parse_mac48(black_box("a4:83:e7:9c:1d:42")).unwrap();
             black_box(classify(m))
         })
+    });
+
+    c.bench_function("scope_global_unicast", |b| {
+        let m = parse_mac48("a4:83:e7:9c:1d:42").unwrap();
+        b.iter(|| black_box(scope(black_box(m))))
+    });
+
+    c.bench_function("scope_ipv4_multicast", |b| {
+        let m = parse_mac48("01:00:5e:00:00:01").unwrap();
+        b.iter(|| black_box(scope(black_box(m))))
+    });
+
+    c.bench_function("lookup_oui_octets", |b| {
+        b.iter(|| black_box(lookup_oui_octets(black_box([0xA4, 0x83, 0xE7]))))
     });
 
     c.bench_function("lookup_unknown", |b| {
